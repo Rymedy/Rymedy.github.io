b1dfeb2cec69a07f904b8909604bce7590a9b300
Rymedy <49023843+Rymedy@users.noreply.github.com>
2019-05-10 12:24:56 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -59,8 +59,8 @@ drone.on('error', error => {
 });
 
 function getRandomName() {
-  const adjs = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
-  const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
+  const adjs = ["im a", "whenyouseeagabby", "zackowacko", "cool", "thicc", "thot", "twitch", "fortniteplayer", "soggy", "itchy", "slobby", "gay", "white", "imlagging", "gtgnowcya", "weaboo", "disabled", "69", "minecrafter"];
+  const nouns = ["thanos", "peepee", "dogereedoo", "pocoloco", "theinsertname", "rymedy", "avrolancaster", "anvatri", "spotlightmoonlight", "gamingaddict", "highschooldxd", "mimimimimi", "bully", "leaf", "nazi", "hitler"];
   return (
     adjs[Math.floor(Math.random() * adjs.length)] +
     "_" +
